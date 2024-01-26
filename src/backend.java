9d86dbf2c1928a187817fd49d44042d720a543f5
Trenton Butler <boochbutler3@gmail.com>
2024-01-26 09:53:05 -0500

    Added backend and frontend classes that extend the programmer class.

---
+++ src/backend.java
@@ -0,0 +1,11 @@
+package src;
+
+public class backend extends programmer
+{
+
+public void progressMade()
+{
+System.out.println("Backend progress has been made");
+}
+
+}
---
+++ src/frontend.java
@@ -0,0 +1,11 @@
+package src;
+
+public class frontend extends programmer
+{
+
+public void progressMade()
+{
+System.out.println("Frontend progress had been made");
+}
+
+}
